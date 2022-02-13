da7a7337c9079c8a5badf04802edeaddb27c8500
StasonLV <talemane@gmail.com>
2022-02-13 17:57:43 +0300

    Initial Commit

---
+++ WeatherTraining App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeatherTraining App
+//
+//  Created by Кристина on 13.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
